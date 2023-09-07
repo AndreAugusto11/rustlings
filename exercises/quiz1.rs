e5fb6e6321005e4cf36267c2b25724f5d96ba48b
André Augusto <andre.augusto@tecnico.ulisboa.pt>
2023-09-07 22:44:48 +0100

    Add solutions to intro, variables, primitive_types, if, functions, quiz1
    Signed-off-by: André Augusto <andre.augusto@tecnico.ulisboa.pt>

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples: i32) -> i32{
+    let mut apple_price = 2;
+
+    if (num_apples > 40) {
+        apple_price = 1;
+    }
+
+    apple_price * num_apples
+}
 
 // Don't modify this function!
 #[test]
